936e92aafbc8b9e6fcb111966f1e9dacf4280382
Kathy Zhang <112294375+b27b@users.noreply.github.com>
2024-09-29 15:06:18 -0400

    fixed hero img sizing issue

--- uwrl/app/pages/home/page.js
+++ uwrl/app/pages/home/page.js
@@ -15,20 +15,15 @@ export default function Home() {
         <div
           className="
         hero-main 
-        bg-[url('/images/assets/iso-grid-hero.png')] bg-bck-white 
-        grid grid-rows-1 grid-cols-1 relative 
-        rounded-2xl"
+        bg-[url('/images/assets/iso-grid-hero.png'),_url('/images/assets/city.png')] bg-bck-white bg-no-repeat bg-bottom
+        relative 
+        rounded-2xl
+        flex justify-center items-center flex-col gap-7"
         >
-          <img
-            src="/images/assets/city.png"
-            className="hero-img col-span-full row-span-full"
-          />
-
-          <div className="flex justify-center items-center flex-col gap-7 col-span-full row-span-full mb-36">
             <span className="font-suse font-bold text-body">
               VR/AR/XR - Spatial Computing
             </span>
-            <div className="flex flex-col gap-6 items-center">
+            <div className="flex flex-col gap-6 items-center mb-36">
               <h1 className="font-neraphic text-h1 w-990 text-center">
                 The University of Waterloo's XR and spatial computing design
                 team.
@@ -39,7 +34,6 @@ export default function Home() {
                 icon="/images/icons/arrow-right-white.png"
               />
             </div>
-          </div>
         </div>
       </main>
       <Footer/>
