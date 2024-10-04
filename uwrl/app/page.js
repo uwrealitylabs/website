477d43a794c65c401e463cf74f48eafa44bba20b
Kathy Zhang <112294375+b27b@users.noreply.github.com>
2024-10-04 01:04:50 -0400

    Update page.js

--- uwrl/app/page.js
+++ uwrl/app/page.js
@@ -1,8 +1,5 @@
 import Home from "./pages/home/page"
-import About from "./pages/about/page"
-import Contact from "./pages/contact/page"
-import Projects from "./pages/projects/page"
-import Error from "./pages/error/page"
+
 
 export default function app(){
   return(
