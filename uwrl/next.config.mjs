72873c19d70a0d89eac0a8be442a8a37bfa8a991
Kathy Zhang <112294375+b27b@users.noreply.github.com>
2024-10-04 15:29:43 -0400

    Update next.config.mjs

--- uwrl/next.config.mjs
+++ uwrl/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/uwrl",
+  output: "export",
+  reactStrictMode: true,
+};
 
-export default nextConfig;
+module.exports = nextConfig;
