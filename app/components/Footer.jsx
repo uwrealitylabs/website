7d420db2c52f66e37eedaa655274715fb441b224
Kathy Zhang <112294375+b27b@users.noreply.github.com>
2024-10-10 14:58:17 -0400

    update footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -6,31 +6,18 @@ export const metadata = {
 export default function Footer() {
   return (
     <footer
-      className=" w-full h-830 bg-cover bg-no-repeat bg-bottom bg-[url('/images/assets/city-footer.png')] bg-bck-grey flex 
-    
-    max-[1700px]:justify-center
-     
-     "
-    >
+      className=" w-full h-830 bg-cover bg-no-repeat bg-bottom bg-[url('/images/assets/city-footer.png')] bg-bck-grey flex || max-[1700px]:justify-center">
       <div
         className="flex gap-32 mt-48 ml-48 
-      
       ---1536px---
       max-2xl:mx-24 max-2xl:mt-28
       max-[1700px]:mr-72
-
-      
       ---768px---
       max-md:flex-col max-md:gap-12 max-md:mx-16 max-md:mt-20
 
       max-lg:gap-10
-
       ---520px---
-      max-mobile:gap-6  max-mobile:mt-16
-
-      
-      "
-      >
+      max-mobile:gap-6  max-mobile:mt-16">
         {/* main body of text   */}
         <div className="flex flex-col gap-12">
           <h1
@@ -66,7 +53,7 @@ export default function Footer() {
         <nav className="flex gap-12 ">
           <div className="flex flex-col gap-2">
             <a
-              href="#"
+              href="/"
               className="font-suse text-text-lightBody text-xl
               hover:text-text-lightSub duration-300
               "
@@ -80,6 +67,14 @@ export default function Footer() {
               "
             >
               Projects
+            </a>
+            <a
+              href="/blogs"
+              className="font-suse text-text-lightBody text-xl              
+              hover:text-text-lightSub duration-300
+              "
+            >
+              Blogs
             </a>
             <a
               href="/about"
