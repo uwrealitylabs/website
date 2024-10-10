cab045ce2ee8a861ab61c3d94579cdb2ff423d55
Kathy Zhang <112294375+b27b@users.noreply.github.com>
2024-10-10 16:29:06 -0400

    northstar project page

--- app/(pages)/projects/(projects)/northstar/page.js
+++ app/(pages)/projects/(projects)/northstar/page.js
@@ -2,18 +2,25 @@ import NavBar from "@/app/components/NavBar";
 import Footer from "@/app/components/Footer";
 import ImageHeader from "../../../../components/ImageHeading";
 import ProjDescrip from "../../ProjDescrip";
+import ContentImages from "../../ContentImages";
+import JoinUs from "@/app/components/JoinUs";
+import BackButton from "@/app/components/BackButton";
 
-import NorthStar from "@/public/images/photos/northstar/northstar.png"
+import NorthStar from "@/public/images/photos/northstar/northstar.png";
+import FrontView from "@/public/images/photos/northstar/frontview.png";
+import OnHead2 from "@/public/images/photos/northstar/onhead2.png";
+import PCB from "@/public/images/photos/northstar/pcb.png";
+import TopView from "@/public/images/photos/northstar/topview.png";
 
 export default function Northstar() {
   return (
     <div className="bg-[url('/images/assets/grid.png')] bg-bck-white">
       <NavBar />
       <main className="px-24 py-6 grid gap-y-16 || max-lg:px-12  || max-mobile:px-4">
-        <ImageHeader
-          title="Project North Star"
-          img={NorthStar}
-        />
+        <div>
+          <BackButton />
+          <ImageHeader title="Project North Star" img={NorthStar} />
+        </div>
 
         <ProjDescrip
           projecttype="AR Hardware"
@@ -42,8 +49,16 @@ export default function Northstar() {
               voice coils.
             </p>,
           ]}
-        //   link="https://github.com/uwrealitylabs/realityfromscratch"
+          //   link="https://github.com/uwrealitylabs/realityfromscratch"
         />
+
+        <section className="mobile:grid mobile:grid-cols-2 mx-auto max-mobile:flex max-mobile:flex-col gap-6 my-16">
+          <ContentImages photo={TopView} grid="full" />
+          <ContentImages photo={PCB} />
+          <ContentImages photo={FrontView} />
+          <ContentImages photo={OnHead2} grid="full" />
+        </section>
+        <JoinUs />
       </main>
       <Footer />
     </div>
--- app/(pages)/projects/ContentImages.jsx
+++ app/(pages)/projects/ContentImages.jsx
@@ -4,9 +4,10 @@ export default function ContentImages({ photo, grid="" }) {
   return (
     <Image
       alt=""
-      src={`/images/photos/${photo}`}
-      className={`max-mobile:w-full max-mobile:aspect-4/3 object-cover
-        ${grid === "full" ? "col-span-full" : ""}
+      src={photo}
+      className={`max-mobile:w-full max-mobile:aspect-4/3 object-cover rounded-2xl w-full
+        ${grid === "" ? "aspect-4/3" : ""}
+        ${grid === "full" ? "col-span-full aspect-16/9" : ""}
         `}
     />
   );
