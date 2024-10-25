fa41f5f2312efef35bd5ec85c6a7b1b8656859f2
Kathy Zhang <112294375+b27b@users.noreply.github.com>
2024-10-25 13:16:29 -0400

    update northstar

--- app/(pages)/projects/(projects)/northstar/page.js
+++ app/(pages)/projects/(projects)/northstar/page.js
@@ -25,29 +25,9 @@ export default function Northstar() {
         <ProjDescrip
           projecttype="AR Hardware"
           details="A fully self-contained AR device based on Ultraleap's Project North Star"
-          heading="The modular, open-source VR headset for research and experimentation."
+          heading=""
           description={[
-            <p key="9">
-              We are building our very own, fully custom VR headset. We call it
-              Reality from Scratch.
-            </p>,
-
-            <p key="10">
-              We first soldered an inertial measurement unit (IMU) and
-              microcontroller unit (MCU) together, and got real-time motion
-              vector data translated into SteamVR with drivers forked from the
-              OpenVR SDK. Then, the VR Compositor output was routed to our VR
-              displays, which comes with accompanying lenses and a custom
-              3D-printed housing. In addition to the HMD, we are building Vive
-              Wand-style controllers, which we&apos;ll show below.
-            </p>,
-
-            <p key="12">
-              From this headset, we plan to build other systems, such as an
-              inside-out 6DoF tracking solution using visual-inertial odometry,
-              or a varifocal optical stack using eye tracking and motors or
-              voice coils.
-            </p>,
+  
           ]}
           //   link="https://github.com/uwrealitylabs/realityfromscratch"
         />
